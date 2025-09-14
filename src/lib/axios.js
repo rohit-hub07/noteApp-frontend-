f8452ed2faa268e5e28e790af92a961c11c42308
rohitsingh <rohitsingh986491@gmail.com>
2025-09-15 00:59:39 +0530

    issue

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8000"
-      : "https://backend-leza905b8-rohit-hub07s-projects.vercel.app",
+      : "https://backend-self-one-83.vercel.app",
   withCredentials: true,
 });
 
